3c7c44a8850b8b51bb2fd1e5fac6d97f9d1ec68a
Nikos Giannopoulos <n.giannopoulos@jrtechnologies.com>
2018-08-02 14:40:56 +0300

    After Section 8.

--- src/main/java/com/jrt/controllers/IndexController.java
+++ src/main/java/com/jrt/controllers/IndexController.java
@@ -1,14 +1,24 @@
 package com.jrt.controllers;
 
+import com.jrt.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"", "/", "index"})
-    public String getIndexPage() {
-        System.out.println("Some message to say...");
+    public String getIndexPage(Model model) {
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
---
+++ src/main/java/com/jrt/services/RecipeService.java
@@ -0,0 +1,10 @@
+package com.jrt.services;
+
+import com.jrt.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
