5d2467ab1eef510a21339d7845e14c14b7aa458d
Nikos Giannopoulos <n.giannopoulos@jrtechnologies.com>
2018-08-02 11:31:15 +0300

    After Lecture 125.

--- src/main/java/com/jrt/domain/Recipe.java
+++ src/main/java/com/jrt/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.jrt.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -16,8 +17,12 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
     //todo add
     //private Difficulty difficulty'
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -103,5 +108,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
