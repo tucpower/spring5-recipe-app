26435265a3b2f58b40588cf2e9d3ec32ca61918b
Nikos Giannopoulos <n.giannopoulos@jrtechnologies.com>
2018-08-06 16:37:01 +0300

    After Lecture 175.

--- src/main/java/com/jrt/services/RecipeServiceImpl.java
+++ src/main/java/com/jrt/services/RecipeServiceImpl.java
@@ -5,6 +5,7 @@ import com.jrt.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -21,5 +22,17 @@ public class RecipeServiceImpl implements RecipeService {
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
+    }
+
+    @Override
+    public Recipe findById(Long l) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+
+        if(!recipeOptional.isPresent()) {
+            throw new RuntimeException("Recipe Nor Found!");
+        }
+
+        return recipeOptional.get();
     }
 }
